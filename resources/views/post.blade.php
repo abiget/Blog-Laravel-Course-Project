f9b7a4f9621d15389f29915da140a4cf1db55438
abiget <getachewante@gmail.com>
2023-06-09 23:40:04 +0300

    blade layout added

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+<x-layout>
     <article>
         <h1>
             {{ $post->title }}
@@ -19,5 +10,4 @@
     </article>
 
     <a href="/">Go Back</a>
-</body>
-</html>
+</x-layout>
