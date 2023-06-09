b75f595c224abcedc4b9f666419b6e49532178ff
abiget <getachewante@gmail.com>
2023-06-09 19:54:19 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    //Get all posts and pass them to a view called 'posts'
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
+});
+
+Route::get('/posts/{post}', function ($slug){
+    // Find a post by its slug and pass it to a view called 'post'
+    return view('post',[
+        'post'=> Post::find($slug)
+    ]);
+})->where('post', '[A-z_\-]+');
