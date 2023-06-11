c519f1d907d2654793bf62fffb685eb9a3011411
abiget <getachewante@gmail.com>
2023-06-11 22:55:54 +0300

    add front-end.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,8 +15,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
 
+        $user = User::factory()->create([
+            'name' => 'John Doe'
+        ]);
+        Post::factory(5)->create([
+            'user_id' => $user->id
+        ]);
+        // $user = User::factory()->create();
+
+        // $personal = Category::create([
+        //     'name'=>'Personal',
+        //     'slug'=>'personal'
+        // ]);
+
+        // $family = Category::create([
+        //     'name'=>'Family',
+        //     'slug'=>'family'
+        // ]);
+
+        // $work = Category::create([
+        //     'name'=>'Work',
+        //     'slug'=>'work'
+        // ]);
+
+        // Post::create([
+        //     'user_id'=>$user->id,
+        //     'category_id'=>$family->id,
+        //     'title'=>'My Family Post',
+        //     'slug'=>'my-family-post',
+        //     'excerpt'=>'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>',
+        //     'body'=>'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec rutrum tellus. Donec convallis sapien vel elit consectetur, vel rutrum ligula auctor. Maecenas semper magna eget eleifend venenatis. Sed non venenatis quam, sit amet egestas odio. Duis feugiat pulvinar dui, nec posuere lacus vestibulum id. In ultricies commodo massa, laoreet venenatis mauris porta non. Vestibulum aliquam massa eget ligula dictum rutrum. Nulla quis elit eros. Vivamus lectus est, elementum bibendum euismod nec, eleifend quis turpis. Nunc lobortis, urna in maximus cursus, erat augue porttitor lorem, nec vulputate est eros quis ante. Suspendisse feugiat elementum leo nec ultrices. Curabitur nec commodo felis.</p>'
+        // ]);
+
+        // Post::create([
+        //     'user_id'=>$user->id,
+        //     'category_id'=>$work->id,
+        //     'title'=>'My Work Post',
+        //     'slug'=>'my-work-post',
+        //     'excerpt'=>'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>',
+        //     'body'=>'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec rutrum tellus. Donec convallis sapien vel elit consectetur, vel rutrum ligula auctor. Maecenas semper magna eget eleifend venenatis. Sed non venenatis quam, sit amet egestas odio. Duis feugiat pulvinar dui, nec posuere lacus vestibulum id. In ultricies commodo massa, laoreet venenatis mauris porta non. Vestibulum aliquam massa eget ligula dictum rutrum. Nulla quis elit eros. Vivamus lectus est, elementum bibendum euismod nec, eleifend quis turpis. Nunc lobortis, urna in maximus cursus, erat augue porttitor lorem, nec vulputate est eros quis ante. Suspendisse feugiat elementum leo nec ultrices. Curabitur nec commodo felis.</p>'
+        // ]);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\File;
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 /*
@@ -15,17 +16,31 @@ use App\Models\Post;
 */
 
 Route::get('/', function () {
-    //Get all posts and pass them to a view called 'posts'
-    $posts = Post::all();
+    //Get all posts and pass them to a view called 'posts
+    $posts = Post::latest()->with(['category', 'author'])->get();
 
     return view('posts', [
         'posts' => $posts
     ]);
 });
 
-Route::get('/posts/{post}', function (Post $post){
+Route::get('posts/{post:slug}', function (Post $post){
     // Find a post by its id and pass it to a view called 'post'
     return view('post',[
         'post'=> $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category){
+    //find posts associated with a category by its id and pass it to a view called 'posts'
+    return view('posts', [
+        'posts' => $category->posts->load(['author', 'category'])
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author){
+    //find posts associated with a category by its id and pass it to a view called 'posts'
+    return view('posts', [
+        'posts' => $author->posts->load(['author', 'category'])
     ]);
 });
