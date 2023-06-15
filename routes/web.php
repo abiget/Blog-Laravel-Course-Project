89c751f7cf1122170c1288b010298e631db65f0c
abiget <getachewante@gmail.com>
2023-06-15 22:34:40 +0300

    add register and session controller flash message logout.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\User;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,6 +18,11 @@ use App\Models\Post;
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+Route::post('logout', [SessionsController::class, 'destroy']);
+
 
 // Route::get('categories/{category:slug}', function (Category $category){
 //     //find posts associated with a category by its id and pass it to a view called 'posts'
