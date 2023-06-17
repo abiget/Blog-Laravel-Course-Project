1f85c0739ceb01cfe1c680fb6d1e4a4ba2474967
abiget <getachewante@gmail.com>
2023-06-17 12:36:51 +0300

    panel style comment section.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -49,6 +49,28 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <x-panel>
+                        <form action="" method="post">
+                            @csrf
+    
+                            <header class="flex items-center">
+                                <img src="https://i.pravatar.cc/40?u={{ auth()->id() }}" alt="The image of the author avator." 
+                                     width="40" height="40" class="rounded-full"
+                                >
+    
+                                <h2 class="ml-4">Want to participate?</h2>
+                            </header>
+    
+                            <div class="mt-6">
+                                <textarea name="" class="w-full text-sm focus:outline-none focus:ring" id="" cols="30" rows="5" placeholder="Quick, thing of something to say!"></textarea>
+                            </div>
+    
+                            <div class="flex justify-end border-t border-gray-200 mt-6 pt-6">
+                                <button class="bg-blue-500 font-semibold hover:bg-blue-600 px-10 py-2 rounded-2xl text-white text-xs uppercase" type="submit">Post</button>
+                            </div>
+                        </form>
+                    </x-panel>
+
                     @foreach ($post->comments as $comment)
                         <x-post-comment :comment="$comment" />
                     @endforeach
